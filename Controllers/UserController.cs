a6947ab9d1731b6ac1eb8f315e5ef0ad8d054d90
github_cloudlabsuser_2092@cloudlabsaiuser.com <github_cloudlabsuser_2092@cloudlabsaiuser.com>
2024-06-14 05:46:12 +0000

    methods fro class usercontroller inplemented.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,66 +1,109 @@
-using CRUD_application_2.Models;
+using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
- 
+using CRUD_application_2.Models; // Replace 'YourApp' with the actual namespace of your User model
+
 namespace CRUD_application_2.Controllers
 {
     public class UserController : Controller
     {
-        public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
+        // Static list to simulate database
+        static List<User> userlist = new List<User>();
+
         // GET: User
         public ActionResult Index()
         {
-            // Implement the Index method here
+            return View(userlist);
         }
- 
+
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            return View();
         }
- 
+
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            try
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
- 
+
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            try
+            {
+                var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+                if (existingUser == null)
+                {
+                    return HttpNotFound();
+                }
+                // Update properties here
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                // Add more fields as necessary
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
- 
+
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
         {
-            // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user != null)
+            {
+                userlist.Remove(user);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
